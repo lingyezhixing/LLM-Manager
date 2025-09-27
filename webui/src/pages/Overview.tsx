ea4ed445d8ceb5a016d857516e9c1aaa21b5eeaf
lingyezhixing <3105701479@qq.com>
2025-09-27 21:13:58 +0800

    实现总览页对模型吞吐量的监控组件

--- webui/src/pages/Overview.tsx
+++ webui/src/pages/Overview.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DeviceMonitor from '../components/DeviceMonitor'
+import ProgramStatusMonitor from '../components/ProgramStatusMonitor'
 
 const Overview: React.FC = () => {
   return (
     <div className="overview-page">
       <DeviceMonitor />
+      <ProgramStatusMonitor />
     </div>
   )
 }
